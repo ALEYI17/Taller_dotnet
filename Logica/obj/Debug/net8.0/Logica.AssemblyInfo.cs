45796f0185d4fa774f31fd32eff2c6d018a442a0
Andres Salamanca <salamanca-andres@javeriana.edu.co>
2024-05-09 16:11:46 -0500

    terminado cleitne rest y Datos rest

--- Logica/obj/Debug/net8.0/Logica.AssemblyInfo.cs
+++ Logica/obj/Debug/net8.0/Logica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae515f7015131eb0335a22793f5ab4f3f4502b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
