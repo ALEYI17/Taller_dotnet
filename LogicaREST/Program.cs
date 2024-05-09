30c515fd250275b7d854ef2c5b1facbcc0a16b3d
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-05-09 12:58:08 -0500

    Feat: Implementa GRPC

--- LogicaREST/Program.cs
+++ LogicaREST/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => {
+    // La dirección del servidor gRPC
+    using var channel = GrpcChannel.ForAddress("http://localhost:5243");
+
+    // Crear el cliente gRPC
+    var client = new DataBuff.DataBuffClient(channel);
+
+    // Ejemplo de llamada al método del servicio gRPC
+    var response = await client.ListUsusarioAsync(new GetAllUsersRequest());
+
+    return response;
+});
 
 app.Run();
